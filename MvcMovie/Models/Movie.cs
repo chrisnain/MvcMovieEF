78169da9e6caf1adf77e3f5bd6e753245f537108
brueckmann <chrisnain@hotmail.com>
2016-10-26 14:12:30 +0200

    Added EF scaffold for application.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace MvcMovie.Models
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime ReleaseDate { get; set; }
+		public string Genre { get; set; }
+		public decimal Price { get; set; }
+	}
+}
